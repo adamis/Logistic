0946600a27f975a807f9a62f64423d45dbed9e72
adamis <adamisstarling@gmail.com>
2025-04-13 16:26:22 -0300

    Initial commit

---
+++ HelloWorld/src/main/java/br/com/fiserv/api/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.fiserv.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/alive")
+public class HelloWorldController {
+	
+	@GetMapping
+	public String pesquisar() {
+		return "Hello World";
+	}
+	
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Logistic
+ Mono Repo - Maven Multi Module
